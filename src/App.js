75924b522b6948ee615cf0295ee5aff1a3cc0452
naingwinphyoe <naingwinphyoe.19@gmail.com>
2024-08-26 01:42:55 +0630

    final-to-do

--- src/App.js
+++ src/App.js
@@ -5,20 +5,39 @@ import TodoList from './components/TodoList.js';
 import CheckAllAndRemaining from './components/CheckAllAndRemaining.js';
 import TodoFilters from './components/TodoFilters.js';
 import ClearCompletedBtn from './components/ClearCompletedBtn.js';
-import { useEffect, useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 
 
 function App() {
 
   let [ todos, setTodos] = useState([]);
+  let [ filteredTodos, setFilteredTodos] = useState(todos);
 
   useEffect(() => {
     fetch('http://localhost:3001/todos')
     .then (res => res.json())
     .then((todos) => {
-      setTodos(todos)
+      setTodos(todos);
+      setFilteredTodos(todos);
     })
-  }, [])
+  }, [todos])
+
+  let filterBy = useCallback((filter) => {
+    // ref datatype use useEffect put depcy array infinite loop so useCallback fun use
+    if(filter === 'All'){
+      setFilteredTodos(todos);
+      // show all todo
+    }
+    if(filter === 'Active'){
+      setFilteredTodos(todos.filter(t => !t.completed)) 
+      // show unfinish todo
+    }
+    if(filter === 'Completed') {
+      setFilteredTodos(todos.filter(t => t.completed))
+      // show finish todo
+    }
+
+  },[todos])
 
 
   let addTodo = (todo) => {
@@ -102,11 +121,11 @@ function App() {
       <div className="todo-app">
         <h2>Todo App</h2>
         <TodoForm addTodo={addTodo}/>
-        <TodoList todos={todos} deleteTodo={deleteTodo} updateTodo={updateTodo}/>
+        <TodoList todos={filteredTodos} deleteTodo={deleteTodo} updateTodo={updateTodo}/>
         <CheckAllAndRemaining remainingCount={remainingCount} checkAll={checkAll}/>
 
         <div className="other-buttons-container">
-        <TodoFilters/>
+        <TodoFilters filterBy={filterBy}/>
         
 
           <ClearCompletedBtn clearCompleted={clearCompleted}/>
